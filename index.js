fd7c677ba4e0f119315703df59187c82fa22e18d
Jaikumarvanjare <Jaikumarvanjare2022@gmail.com>
2026-02-18 11:44:28 +0530

    added movie model

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 const express= require('express');
-const bodyParser = require('body-parser');
-const env= require('dotenv');
+const env= require('dotenv').config();
 const mongoose = require('mongoose');
+const movie = require('./models/movie.model');
 
-env.config();
 const app= express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 app.get('/home', (req,res)=>{
     console.log("hitting /Home");
     return res.json({
         success:true,
         message:"fetched home"
-    })
+    })   
 })
 
 app.listen(process.env.PORT, async() => {
     console.log(`server running on http://localhost:${process.env.PORT}`);
 
     mongoose.connect(process.env.DB_URL)
-        .then(() => {
-            console.log("Connected to MongoDB");
-        })
-        .catch((err) => {
-            console.log("Not able to connect to MongoDB", err);
-        });
+    .then(async () => {
+        console.log("Connected to MongoDB");
+
+        // await movie.create({
+        //     name: "Inception",
+        //     description: "A thief who steals corporate secrets through dream-sharing technology.",
+        //     casts: "Leonardo DiCaprio, Joseph Gordon-Levitt",
+        //     releaseDate: "2010",
+        //     director: "Christopher Nolan"
+        // });
+
+        // console.log("Movie inserted");
+    })
+    .catch((err) => {
+        console.log("Not able to connect to MongoDB", err);
+    });
 });
