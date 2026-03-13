b83e523cb4b36776c06b5ea7f221f44aa4241639
jaikumarvanjare <jaikumarvanjare2022@gmail.com>
2026-03-13 11:31:32 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-const express= require('express');
-const env= require('dotenv').config();
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const env = require('dotenv');
 const mongoose = require('mongoose');
 
-const movieRoutes = require('./routes/movie.routes');
+const MovieRoutes = require('./routes/movie.routes');
 const theatreRoutes = require('./routes/theatre.routes');
 const authRoutes = require('./routes/auth.routes');
 const userRoutes = require('./routes/user.routes');
 const bookingRoutes = require('./routes/booking.routes');
 const showRoutes = require('./routes/show.routes');
 const paymentRoutes = require('./routes/payment.routes');
-const notificationRoutes = require('./routes/notification.routes');
 
-const app= express();
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
+env.config();
+const app = express(); 
 
-// mongoose.set('debug', true);    
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-movieRoutes(app);
-theatreRoutes(app);
-authRoutes(app);
-userRoutes(app);
-bookingRoutes(app);
-showRoutes(app);
+mongoose.set('debug', true);
+
+MovieRoutes(app); 
+theatreRoutes(app); 
+authRoutes(app); 
+userRoutes(app); 
+bookingRoutes(app); 
+showRoutes(app); 
 paymentRoutes(app); 
-notificationRoutes(app);
 
-const startServer = async () => {
+app.get('/', (req, res) => {
+    res.send('Home');
+})
+
+app.listen(process.env.PORT, async () => {
+    console.log(`Server started on Port ${process.env.PORT} !!`);
     try {
-        await mongoose.connect(process.env.DB_URL);
-        console.log("Connected to MongoDB");
-
-        app.listen(process.env.PORT, () => {
-            console.log(`Server running on http://localhost:${process.env.PORT}`);
-        });
-
+        if(process.env.NODE_ENV == 'production') {
+            await mongoose.connect(process.env.PROD_DB_URL); 
+        } else {
+            await mongoose.connect(process.env.DB_URL); 
+        }
+        console.log("Successfully connected to mongo");
     } catch (err) {
-        console.log("Not able to connect to MongoDB", err);
-        process.exit(1);
+        console.log("Not able to connect mongo", err);
     }
-};
-
-startServer();
+});
